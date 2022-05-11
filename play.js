7e8a5f667eeadc52b26dce076af32e10792e10cb
James Ball <jamie.j.ball@gmail.com>
2022-05-11 08:17:27 -0700

    imported connect funct

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
 const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  conn.on("connect", () => {
-    console.log("You have been connected!");
-  });
-  conn.on("disconnect", () => {
-    console.log("You ded");
-  });
-  conn.setEncoding("utf8");
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
