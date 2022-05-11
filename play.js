e1e090b9d3b000f522adcabc10513f6e71ff1172
James Ball <jamie.j.ball@gmail.com>
2022-05-11 08:10:12 -0700

    added connect function

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  conn.on("connect", () => {
+    console.log("You have been connected!");
+  });
+  conn.on("disconnect", () => {
+    console.log("You ded");
+  });
+  conn.setEncoding("utf8");
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
