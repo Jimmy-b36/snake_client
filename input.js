35dafa3936c960574b30b6a4054f30eb487ef0c8
James Ball <jamie.j.ball@gmail.com>
2022-05-11 09:29:49 -0700

    updated input to send commands to server

--- input.js
+++ input.js
@@ -1,11 +1,26 @@
+const { conn } = require("./client");
+
 const handleUserInput = function (data) {
   if (data === "\u0003") {
     console.log("Thanks for playing!");
     process.exit();
   }
+  if (data === "\u0077") {
+    conn.write("Move: up");
+  }
+  if (data === "\u0061") {
+    conn.write("Move: left");
+  }
+  if (data === "\u0073") {
+    conn.write("Move: down");
+  }
+  if (data === "\u0064") {
+    conn.write("Move: right");
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
