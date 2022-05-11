ba4101566e425d2a5c1ce439ec5fe2448ef49356
James Ball <jamie.j.ball@gmail.com>
2022-05-11 08:53:07 -0700

    added exit commands

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
-const net = require("net");
 const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    console.log("Thanks for playing!");
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
